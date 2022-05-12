b715a4d7fea30496b2588918b5c97611700c1b9f
Abolanosup <104039035+Abolanosup@users.noreply.github.com>
2022-05-12 18:40:40 -0400

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -4,7 +4,7 @@ API_HASH = '9d8097d05bbc90a6ed2a7a81abcd4e8a'
 ADMIN_ID = [1161231954]
 
 MOODLE_URL = 'https://evea.uh.cu/'
-MOODLE_USER = 'daynelispalacios'
-MOODLE_PASSWORD = 'Rockstar017337@jwue'
+MOODLE_USER = 'darian.borges@estudiantes.fbio.uh.cu'
+MOODLE_PASSWORD = 'darian1995'
 PARTES_MB = 200
 
